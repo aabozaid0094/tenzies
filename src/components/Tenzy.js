6c39c8c29a869dc588b14f4fb9abddee1d381edf
Ahmed Abo Zaid <aabozaid0094@gmail.com>
2022-11-22 18:57:00 +0200

    Adding freeze handler

--- src/components/Tenzy.js
+++ src/components/Tenzy.js
@@ -1,6 +1,12 @@
-const Tenzy = (props)=>{
-    return(
-        <div className={`tenzy${props.dark_schema?" dark-schema":" light-schema"}${props.freezed?" freezed":""}`}>
+const Tenzy = (props) => {
+    return (
+        <div
+            className={`tenzy${
+                props.dark_schema ? " dark-schema" : " light-schema"
+            }${props.freezed ? " freezed" : ""}`}
+            onClick={()=>props.toggle_tenzy_freeze(props.id)}
+            id={`tenzy_${props.id}`}
+        >
             {props.value}
         </div>
     );
